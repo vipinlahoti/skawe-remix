e331304b8ce9c6d346ea641aecb32ee11b3c4216
vipin.lahoti <vipi.nsl2787@gmail.com>
2022-03-15 12:12:17 +0530

    init commit

--- app/i18n.server.js
+++ app/i18n.server.js
@@ -1,9 +1,10 @@
 import { RemixI18Next, FileSystemBackend } from "remix-i18next";
+import path from "path";
 import i18nextOptions from "./i18nextOptions";
 
 // You will need to provide a backend to load your translations, here we use the
 // file system one and tell it where to find the translations.
-const backend = new FileSystemBackend("./app/i18n");
+const backend = new FileSystemBackend(path.resolve("./public/locales"));
 
 export default new RemixI18Next(backend, {
   fallbackLng: i18nextOptions.fallbackLng, // here configure your default (fallback) language
